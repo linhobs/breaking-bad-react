26fbd0b70bfc6e511b12518a120534743a11838d
linhobs <gyamenahyawpius@gmail.com>
2020-07-21 11:49:43 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/ui/Header";
+import axios from "axios";
+import { config } from "./config/config";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
-function App() {
+  //useEffect works like component did mount. fires off when the component is loaded
+  useEffect(() => {
+    //using another fxn because we want to make use of async await
+
+    const fetchItems = async () => {
+      const result = await axios(`${config.baseURL}?name=${query}`);
+
+      console.log("api results", result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    //call fetchItems to trigger it
+    fetchItems();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+export const config = {
+  baseURL: `https://www.breakingbadapi.com/api/characters`,
+};
